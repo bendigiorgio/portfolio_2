069945e1061dfe116ca846169cf0cc2d29ae3b93
Re3xEs <bendigiorgio@gmail.com>
2022-11-01 22:29:51 +0900

    Show globe

---
+++ src/components/travelGlobe/TravelGlobe.js
@@ -0,0 +1,16 @@
+import "./TravelGlobe.scss";
+import * as THREE from 'three';
+import {useEffect} from 'react';
+
+
+export default function TravelGlobe() {
+    
+
+
+    return (
+      <div>
+        
+      </div>  
+        
+    );
+}
